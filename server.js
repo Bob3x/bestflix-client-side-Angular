c9a9bc277d88e6caa12936a2e637c16c0dddea01
Bob3x <bob3xx@protonmail.com>
2024-12-03 23:03:09 +0200

    Fix paths cases

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require("path");
 const app = express();
 
 // Serve static files from the dist directory
-app.use(express.static(__dirname + "/dist/myFlix-Angular-client"));
+app.use(express.static(__dirname + "/dist/my-flix-angular-client"));
 
 // Send all requests to index.html
 app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname + "/dist/myFlix-Angular-client/index.html"));
+    res.sendFile(path.join(__dirname + "/dist/my-flix-angular-client/index.html"));
 });
 
 // Default Heroku port
